936f0cff56c6421ae6eb41d12f2cb2d455cc9d2b
j.faassen <j.faassen@linkorb.com>
2016-11-06 11:54:20 +0100

    Allow less arguments than properties

--- src/Constructor/ProtectedConstructorTrait.php
+++ src/Constructor/ProtectedConstructorTrait.php
@@ -7,9 +7,13 @@ trait ProtectedConstructorTrait
     public function __construct()
     {
         $i = 0;
-        foreach (get_object_vars($this) as $key => $value) {
-            $this->$key = func_get_arg($i);
-            $i++;
+        $vars = get_object_vars($this);
+        $args = func_get_args();
+        foreach ($vars as $key => $value) {
+            if ($i<count($args)) {
+                $this->$key = func_get_arg($i);
+                $i++;
+            }
         }
     }
 }
